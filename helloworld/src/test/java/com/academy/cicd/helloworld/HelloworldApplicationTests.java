90545b174795bd48086f4812b0596d274496fc19
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-03-04 12:26:26 -0300

    Hello world

---
+++ helloworld/src/test/java/com/academy/cicd/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.academy.cicd.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		assertTrue(false);
+	}
+
+}
